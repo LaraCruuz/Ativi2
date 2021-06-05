b8989cf036040fc6d875f19481f524872502c937
LaraCruuz <75038320+LaraCruuz@users.noreply.github.com>
2021-06-04 23:59:09 -0300

    Create trabDisci.java

---
+++ trabDisci.java
@@ -0,0 +1,29 @@
+public class Disciplina{
+ public String Aluno;
+ public int alunos;
+ public String aluno;
+ public String nome;
+ public int id;
+ public int posicao;
+ public double mediaN1;
+ public double nota1;
+ public double mediaN2;
+ public double nota2;
+ 
+    public void main(String[] args){
+ Aluno[] alunos = new Aluno[10];
+  }
+   public void adicionaAluno(String nome, int id, int posicao){
+    this.nome=nome;
+    this.id=id;
+    this.posicao=posicao;
+    }
+    public void mediaN1(){
+    mediaN1 = nota1;
+     System.out.println(mediaN1);
+    }
+    public void mediaN2(){
+    mediaN2 = nota2;
+     System.out.println(mediaN1);
+    }
+}
